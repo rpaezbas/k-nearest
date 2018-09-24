cd2df4df48da7275a922504e31510cb4a81ab8ee
Rafael Paez Bastida <rpaezbas@everis.com>
2018-09-24 17:39:22 +0200

    removed sysout

--- src/test/java/com/rpaezbas/knearest/DataSetTest.java
+++ src/test/java/com/rpaezbas/knearest/DataSetTest.java
@@ -27,15 +27,11 @@ public class DataSetTest extends TestCase {
 	public void setUp() {
 
 		vectors = new ArrayList<Vector>();
-
 		vectors.add(new Vector(new float[] { 2.0F, 2.0F }));
 		vectors.add(new Vector(new float[] { 1.0F, 15.0F }));
 		vectors.add(new Vector(new float[] { 5.0F, 3.0F }));
-
 		dataSet = new DataSet(vectors);
-
 		target = new Vector(new float[] { 4.0F, 3.0F });
-
 		epsilon = 0.001F;
 
 	}
@@ -44,17 +40,11 @@ public class DataSetTest extends TestCase {
 
 		float[] distances = dataSet.calculateDistances(target);
 
-		for (int i = 0; i < distances.length; i++) {
-			System.out.println(distances[i]);
-		}
-
-		assert (distances.length == vectors.size());
-
+		assertEquals (distances.length, vectors.size());
 		assertEquals(distances[0],
 				Math.sqrt(Math.pow(target.getValues()[0] - dataSet.getVectors().get(0).getValues()[0], 2)
 						+ Math.pow(target.getValues()[1] - dataSet.getVectors().get(0).getValues()[1], 2)),
 				epsilon);
-
 		assertEquals(distances[1],
 				Math.sqrt(Math.pow(target.getValues()[0] - dataSet.getVectors().get(1).getValues()[0], 2)
 						+ Math.pow(target.getValues()[1] - dataSet.getVectors().get(1).getValues()[1], 2)),
@@ -64,10 +54,6 @@ public class DataSetTest extends TestCase {
 	public void testCalculateNearest() {
 
 		ArrayList<Vector> nearestVectors = dataSet.calculateNearest(target, 3);
-		
-		for (int i = 0; i < nearestVectors.size(); i++) {
-			System.out.println(nearestVectors.get(i).getValues()[0]);
-		}
 		
 		assertEquals(nearestVectors.get(0).getValues()[0],5.0F,epsilon); //The nearest point to 4,3 is 5,3
 		assertEquals(nearestVectors.get(1).getValues()[0],2.0F,epsilon); //The second is 2,2
