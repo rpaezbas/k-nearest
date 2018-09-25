57f5a701aaf19efad7fec7cb51cf456b558c98d5
Rafael Paez Bastida <rpaezbas@everis.com>
2018-09-25 16:36:42 +0200

    refactored packaging

--- src/main/java/dataset/DatasetFacade.java
+++ src/main/java/dataset/DatasetFacade.java
@@ -1,4 +1,4 @@
-package rest;
+package dataset;
 
 import javax.persistence.Entity;
 import vector.Vector;
--- src/main/java/rest/Resource.java
+++ src/main/java/rest/Resource.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import dataset.DataSet;
-import exceptions.VectorInitializationException;
+import dataset.DatasetFacade;
 import exceptions.calculateNearestException;
 import vector.Vector;
 
